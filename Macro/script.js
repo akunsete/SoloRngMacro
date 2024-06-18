e922607b42c508db2a756ef7f904ea8b35b06201
akunsete <161056036+akunsete@users.noreply.github.com>
2024-06-18 08:16:22 +0700

    Add files via upload

---
+++ Macro/script.js
@@ -0,0 +1,7 @@
+document.getElementById('mobile-btn').addEventListener('click', function() {
+    alert('Coming Soon');
+});
+
+document.getElementById('pc-btn').addEventListener('click', function() {
+    window.location.href = 'pc-macro.html';
+});
